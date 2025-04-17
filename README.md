13a4ca519e6f77b2799ee1c5bed35d86e12f4faf
Aleksandar Ristic <storiaca@gmail.com>
2025-04-17 16:02:01 +0200

    Add code for movie db

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 ## Diagrams
 
-https://app.diagrams.net/
+<https://app.diagrams.net/>
 
 ## Movie DB clip
 
-https://www.youtube.com/watch?v=HYq3IL1ii70&t=9s
+<https://www.youtube.com/watch?v=HYq3IL1ii70&t=9s>
 
 ## Database star link
 
-https://www.databasestar.com/vip/
+<https://www.databasestar.com/vip/>
 
 ## Database star link to github movie sql
 
-https://github.com/bbrumm/databasestar/blob/main/videos/139_movie/create_movie_db.sql
+<https://github.com/bbrumm/databasestar/blob/main/videos/139_movie/create_movie_db.sql>
--- movies/movies.sql
+++ movies/movies.sql
@@ -1,55 +1,123 @@
 CREATE TABLE directors (
-director_id int(11) NOT NULL AUTO_INCREMENT,
-director_name varchar(500),
-created_at timestamp NOT NULL DEFAULT current_timestamp(),
-CONSTRAINT pk_directors PRIMARY KEY (director_id)
+    director_id int(11) NOT NULL AUTO_INCREMENT,
+    director_name varchar(500),
+    created_at timestamp NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT pk_directors PRIMARY KEY (director_id)
 );
 
 CREATE TABLE movies (
-movie_id int(11) NOT NULL AUTO_INCREMENT,
-director_id int(11) NOT NULL,
-title varchar(200),
-status varchar(20),
-age_rating varchar(5),
-runtime_min int,
-release_date date,
-trailer_link varchar(1000),
-description varchar(1000),
-banner_text varchar(1000),
-header_image varchar(1000),
-poster_image varchar(1000),
-synopsis_image varchar(4000),
-created_at timestamp NOT NULL DEFAULT current_timestamp(),
-CONSTRAINT pk_movies PRIMARY KEY (movie_id),
-CONSTRAINT fk_movie_director FOREIGN KEY (director_id) REFERENCES directors(director_id)
+    movie_id int(11) NOT NULL AUTO_INCREMENT,
+    director_id int(11) NOT NULL,
+    title varchar(200),
+    status varchar(20),
+    age_rating varchar(5),
+    runtime_min int,
+    release_date date,
+    trailer_link varchar(1000),
+    description varchar(1000),
+    banner_text varchar(1000),
+    header_image varchar(1000),
+    poster_image varchar(1000),
+    synopsis_image varchar(4000),
+    created_at timestamp NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT pk_movies PRIMARY KEY (movie_id),
+    CONSTRAINT fk_movie_director FOREIGN KEY (director_id) REFERENCES directors (director_id)
 );
 
 CREATE TABLE cast_member (
-cast_id int(11) NOT NULL AUTO_INCREMENT,
-cast_name varchar(500),
-created_at timestamp NOT NULL DEFAULT current_timestamp(),
-CONSTRAINT pk_cast PRIMARY KEY (cast_id)
+    cast_id int(11) NOT NULL AUTO_INCREMENT,
+    cast_name varchar(500),
+    created_at timestamp NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT pk_cast PRIMARY KEY (cast_id)
 );
 
 CREATE TABLE movie_cast (
-movie_id int(11) NOT NULL,
-cast_id int(11) NOT NULL,
-created_at timestamp NOT NULL DEFAULT current_timestamp(),
-CONSTRAINT fk_mc_movies FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-CONSTRAINT fk_mc_cast FOREIGN KEY (cast_id) REFERENCES cast_member(cast_id)
+    movie_id int(11) NOT NULL,
+    cast_id int(11) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT fk_mc_movies FOREIGN KEY (movie_id) REFERENCES movies (movie_id),
+    CONSTRAINT fk_mc_cast FOREIGN KEY (cast_id) REFERENCES cast_member (cast_id)
 );
 
 CREATE TABLE genres (
-genre_id int(11) NOT NULL AUTO_INCREMENT,
-genre_name varchar(500),
-created_at timestamp NOT NULL DEFAULT current_timestamp(),
-CONSTRAINT pk_genres PRIMARY KEY (genre_id)
+    genre_id int(11) NOT NULL AUTO_INCREMENT,
+    genre_name varchar(500),
+    created_at timestamp NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT pk_genres PRIMARY KEY (genre_id)
 );
 
 CREATE TABLE movie_genre (
-movie_id int(11) NOT NULL,
-genre_id int(11) NOT NULL,
-created_at timestamp NOT NULL DEFAULT current_timestamp(),
-CONSTRAINT fk_mg_movies FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-CONSTRAINT fk_mg_genre FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+    movie_id int(11) NOT NULL,
+    genre_id int(11) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT fk_mg_movies FOREIGN KEY (movie_id) REFERENCES movies (movie_id),
+    CONSTRAINT fk_mg_genre FOREIGN KEY (genre_id) REFERENCES genres (genre_id)
 );
+
+INSERT INTO
+    directors (director_id, director_name)
+VALUES
+    (1, 'Kelsey Mann');
+
+INSERT INTO
+    genres (genre_id, genre_name)
+VALUES
+    (1, 'Adventure'),
+    (2, 'Animation'),
+    (3, 'Comedy'),
+    (4, 'Drama'),
+    (5, 'Family'),
+    (6, 'Fantasy');
+
+INSERT INTO
+    cast_member (cast_id, cast_name)
+VALUES
+    (1, 'Amy Poehler');
+
+INSERT INTO
+    movies (
+        movie_id,
+        director_id,
+        title,
+        status,
+        age_rating,
+        runtime_min,
+        release_date,
+        trailer_link,
+        description,
+        banner_text,
+        header_image,
+        poster_image,
+        synopsis
+    )
+VALUES
+    (
+        1,
+        1,
+        'Inside Out 2',
+        'Tickets On Sale',
+        'CTC',
+        105,
+        '20240613',
+        'https://www.youtube.com/watch?v=CkLiND6qa34',
+        'In "Inside Out 2," Disney and Pixar dive back into Riley"s mind, now a teen, as it faces unexpected changes: new Emotions!',
+        'Dive into Riley"s mind and discover new emotions! Secure your seats for a wild ride through the teen years with Joy, Sadness, Anger, Fear.',
+        'https://www.hoyts.com.au/images/banner103.png',
+        'https://www.hoyts.com.au/images/poster2931.png',
+        'In "Inside Out 2," Disney and Pixar dive back into Riley"s mind, now a teen, as it faces unexpected changes: new Emotions! With Joy, Sadness, Anger, Fear, and Disgust unsure how to handle the arrival of Anxiety and others, voiced by Maya Hawke. Amy Poehler returns as Joy, joined by a stellar cast including Lewis Black and Diane Lane. Directed by Kelsey Mann, produced by Mark Nielsen, and featuring a screenplay by Meg LeFauve and Dave Holstein, "Inside Out 2" promises a captivating journey with a score by Andrea Datzman.'
+    );
+
+INSERT INTO
+    movie_cast (movie_id, cast_id)
+VALUES
+    (1, 1);
+
+INSERT INTO
+    movie_genre (movie_id, genre_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5),
+    (1, 6);

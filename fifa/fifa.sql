3e2451732837b7613982d194bbaa33afa6705d4e
Aleksandar Ristic <storiaca@gmail.com>
2026-01-18 20:15:34 +0100

    Fifa db, teams table

---
+++ fifa/fifa.sql
@@ -0,0 +1,9 @@
+CREATE TABLE teams (
+    team_id int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(200),
+    short_name varchar(20),
+    team_logo varchar(200),
+    created_at timestamp NOT NULL DEFAULT current_timestamp(),
+    updated_at timestamp NOT NULL DEFAULT current_timestamp(),
+    CONSTRAINT pk_teams PRIMARY KEY (team_id)
+);
